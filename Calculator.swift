0428e22ba57bdf9b0198b5127de4a79e7dab0fea
AstroCodes12 <108374340+AstroCodes12@users.noreply.github.com>
2024-09-26 10:45:49 -0700

    Create Calculator.swift

---
+++ Calculator.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+// Menu function to display options to the user
+func showMenu() {
+print("""
+Select an operation:
+1: Add
+2: Subtract
+3: Multiply
+4: Divide
+5: Exponent
+6: Remainder
+""")
+}
+showMenu() //displays menu
